8a07383c2e14b0fc09acc95c3455cfd760774f83
veszeli <mate.veszeli@gmail.com>
2026-01-19 09:08:02 +0100

    tab to  4 spaces

--- src/gas_potential.py
+++ src/gas_potential.py
@@ -2,50 +2,50 @@ import numpy as np
 from abc import ABC, abstractmethod
 
 class ExternalPotential(ABC):
-	@abstractmethod
-	def energy(coordinates):
-		"""
-		Parameters
-		----------
-		coordinates: numpy array, N rows, d columns
+    @abstractmethod
+    def energy(coordinates):
+        """
+        Parameters
+        ----------
+        coordinates: numpy array, N rows, d columns
 
-		Returns
-		-------
-		out: float
-		"""
-		pass
+        Returns
+        -------
+        out: float
+        """
+        pass
 
-	def d_energy(coordinates):
-		"""
-		Partial derivative of potential energy w.r.t. coordinates.
+    def d_energy(coordinates):
+        """
+        Partial derivative of potential energy w.r.t. coordinates.
 
-		Parameters:
-		-----------
-		coordinates: numpy array, N rows, d columns
+        Parameters:
+        -----------
+        coordinates: numpy array, N rows, d columns
 
-		Returns:
-		--------
-		out: numpy array, N rows, d columns
-		"""
-		pass
+        Returns:
+        --------
+        out: numpy array, N rows, d columns
+        """
+        pass
 
 
 class SimpleQuadraticPotential(ExternalPotential):
-	def __init__(self, a):
-		r"""
-		Potential in the form:
-		$U = a/2\sum_{i=1}^N \sum_{k=1}^d r_{ik} r_{ik}$
-		Where N is the number of particles, and d is the dimension.
+    def __init__(self, a):
+        r"""
+        Potential in the form:
+        $U = a/2\sum_{i=1}^N \sum_{k=1}^d r_{ik} r_{ik}$
+        Where N is the number of particles, and d is the dimension.
 
-		Parameters
-		----------
-		a: float
-		"""
-		self.a = a
+        Parameters
+        ----------
+        a: float
+        """
+        self.a = a
 
-	def energy(self, coordinates):
-		return self.a/2*np.sum(np.square(coordinates))
+    def energy(self, coordinates):
+        return self.a/2*np.sum(np.square(coordinates))
 
-	def d_energy(self, coordinates):
-		return self.a*coordinates
+    def d_energy(self, coordinates):
+        return self.a*coordinates
 
